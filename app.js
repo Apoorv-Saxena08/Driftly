9c36fbfbd91140c014764ee06b6ef2b9b2eaf00f
Apoorv Saxena <154752694+Apoorv-Saxena08@users.noreply.github.com>
2025-05-17 22:12:59 +0530

    Server setup

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express=require("express");
 const app=express();
- const mongoose=require("mongoose");
+const mongoose=require("mongoose");
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+// Add this route
+app.get("/", (req, res) => {
+    res.send("Welcome to Driftly!");
+});
+const port = 3000;
+app.listen(port,()=>{
+    console.log("Server is running on port 8080");
 });
